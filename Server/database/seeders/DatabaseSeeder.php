e5e6ef212d406d152e1eff506454c4a4f07f1862
abdulhakim233 <seidabdulhakim961@gmail.com>
2025-03-17 22:51:18 +0300

    first commit

---
+++ Server/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'User',
+            'email' => 'test@example.com',
+            'password' => Hash::make('11111111'),
+        ]);
+
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('12345678'),
+        ]);
+    }
+}
