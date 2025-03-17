84f1048578fa94ca57c3d3d6b315721d488f483f
abdulhakim233 <seidabdulhakim961@gmail.com>
2025-03-17 23:23:23 +0300

    feat: add task creation functionality in TaskController

--- Server/routes/api.php
+++ Server/routes/api.php
@@ -1,12 +1,31 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    // List all tasks (with optional filtering)
+    Route::get('/tasks', [TaskController::class, 'index']);
+
+    // Create a new task
+    Route::post('/tasks', [TaskController::class, 'store']);
+
+    // View a specific task
+    Route::get('/tasks/{id}', [TaskController::class, 'show']);
+
+    // Update a task
+    Route::put('/tasks/{id}', [TaskController::class, 'update']);
+
+    // Delete a task
+    Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
+});
 
 
 
