a074f8c8104194333bfe9906343b56d25f00837d
abdulhakim233 <seidabdulhakim961@gmail.com>
2025-03-17 23:39:36 +0300

    feat: add task assignment endpoint in TaskController

--- Server/routes/api.php
+++ Server/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,26 +10,17 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
-Route::middleware('auth:sanctum')->group(function () {
-
-    Route::get('/tasks', [TaskController::class, 'index']);
-
-
-    Route::post('/tasks', [TaskController::class, 'store']);
-
-
-    Route::get('/tasks/{id}', [TaskController::class, 'show']);
-
-
-    Route::put('/tasks/{id}', [TaskController::class, 'update']);
-
-
-    Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
-});
-
-
-
+// Authentication Routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+// Task Management Routes (protected by Sanctum)
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/tasks', [TaskController::class, 'index']);
+    Route::post('/tasks', [TaskController::class, 'store']);
+    Route::get('/tasks/{id}', [TaskController::class, 'show']);       // Show a specific task
+    Route::put('/tasks/{id}', [TaskController::class, 'update']);     // Update a task
+    Route::delete('/tasks/{id}', [TaskController::class, 'destroy']); // Delete a task
+    Route::put('/tasks/{id}/assign', [TaskController::class, 'assignUser'])->middleware(AdminMiddleware::class); // Assign a user to a task
+});
