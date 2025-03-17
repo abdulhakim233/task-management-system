50fd89b276db98fe602fdcfd84f3d2e0dbec448c
abdulhakim233 <seidabdulhakim961@gmail.com>
2025-03-17 23:43:18 +0300

    feat: add task deletion functionality in TaskController

--- Server/app/Http/Controllers/TaskController.php
+++ Server/app/Http/Controllers/TaskController.php
@@ -90,12 +90,8 @@ class TaskController extends Controller
     // Delete a task
     public function destroy($id)
     {
-        $user = Auth::user();
-        $task = Task::findOrFail($id);
 
-        if ($user->role !== 'admin' && $task->user_id !== $user->id) {
-            return response()->json(['message' => 'Unauthorized'], 403);
-        }
+        $task = Task::findOrFail($id);
 
         $task->delete();
 
