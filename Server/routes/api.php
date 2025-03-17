0c953c59cff0a351a4d6b303b212d9966823f444
abdulhakim233 <seidabdulhakim961@gmail.com>
2025-03-17 23:26:00 +0300

    feat: add retrieval of all tasks in TaskController

--- Server/routes/api.php
+++ Server/routes/api.php
@@ -11,19 +11,19 @@ Route::get('/user', function (Request $request) {
 
 
 Route::middleware('auth:sanctum')->group(function () {
-    // List all tasks (with optional filtering)
+
     Route::get('/tasks', [TaskController::class, 'index']);
 
-    // Create a new task
+
     Route::post('/tasks', [TaskController::class, 'store']);
 
-    // View a specific task
+
     Route::get('/tasks/{id}', [TaskController::class, 'show']);
 
-    // Update a task
+
     Route::put('/tasks/{id}', [TaskController::class, 'update']);
 
-    // Delete a task
+
     Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
 });
 
