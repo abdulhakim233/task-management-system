00bb2f8730a63ada3a610edf2550394ceb1f11e9
abdulhakim233 <seidabdulhakim961@gmail.com>
2025-03-17 23:58:18 +0300

    feat: enhance task retrieval to include assigned tasks for users

--- Server/routes/api.php
+++ Server/routes/api.php
@@ -19,8 +19,8 @@ Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanc
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/tasks', [TaskController::class, 'index']);
     Route::post('/tasks', [TaskController::class, 'store']);
-    Route::get('/tasks/{id}', [TaskController::class, 'show']);       // Show a specific task
-    Route::put('/tasks/{id}', [TaskController::class, 'update']);     // Update a task
+    Route::get('/tasks/{id}', [TaskController::class, 'show']);
+    Route::put('/tasks/{id}', [TaskController::class, 'update']);
     Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
-    Route::put('/tasks/{id}/assign', [TaskController::class, 'assignUser'])->middleware(AdminMiddleware::class); // Assign a user to a task
+    Route::put('/tasks/{id}/assign', [TaskController::class, 'assignUser'])->middleware(AdminMiddleware::class);
 });
